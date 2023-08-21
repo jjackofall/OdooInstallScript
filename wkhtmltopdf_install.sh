9e96aad03882e0c5f8828a13b8ac018072a622d6
Sumit Khanna <sumitkhanna2222@gmail.com>
2023-08-21 23:09:22 +0530

    Create wkhtmltopdf_install.sh

---
+++ wkhtmltopdf_install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+################################################################################
+# Script for installing Wkhtmltopdf on Ubuntu 16.04, 18.04, and 20.04 (could be used for another version too)
+# Author: Sumit Khanna
+#-------------------------------------------------------------------------------
+# This script will install Wkhtmltopdf on your Ubuntu server.
+#-------------------------------------------------------------------------------
+# Make a new file:
+# sudo nano wkhtmltopdf_install.sh
+# Place this content in it and then make the file executable:
+# sudo chmod +x wkhtmltopdf_install.sh
+# Execute the script to install wkhtmltopdf:
+# ./wkhtmltopdf_install
+################################################################################
+
+INSTALL_WKHTMLTOPDF="True"
+
+###  WKHTMLTOPDF download links
+## === Ubuntu Trusty x64 & x32 === (for other distributions please replace these two links,
+## in order to have correct version of wkhtmltopdf installed, for a danger note refer to
+## https://github.com/odoo/odoo/wiki/Wkhtmltopdf ):
+## https://www.odoo.com/documentation/16.0/administration/install.html
+
+# Check if the operating system is Ubuntu 22.04
+if [[ $(lsb_release -r -s) == "22.04" ]]; then
+    WKHTMLTOX_X64="https://packages.ubuntu.com/jammy/wkhtmltopdf"
+    WKHTMLTOX_X32="https://packages.ubuntu.com/jammy/wkhtmltopdf"
+    #No Same link works for both 64 and 32-bit on Ubuntu 22.04
+else
+    # For older versions of Ubuntu
+    WKHTMLTOX_X64="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.$(lsb_release -c -s)_amd64.deb"
+    WKHTMLTOX_X32="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.$(lsb_release -c -s)_i386.deb"
+fi
+
+#--------------------------------------------------
+# Install Wkhtmltopdf if needed
+#--------------------------------------------------
+if [ $INSTALL_WKHTMLTOPDF = "True" ]; then
+  echo -e "\n---- Install wkhtml and place shortcuts on correct place for ODOO 13 ----"
+  #pick up correct one from x64 & x32 versions:
+  if [ "`getconf LONG_BIT`" == "64" ];then
+      _url=$WKHTMLTOX_X64
+  else
+      _url=$WKHTMLTOX_X32
+  fi
+  sudo wget $_url
+  
+
+  if [[ $(lsb_release -r -s) == "22.04" ]]; then
+    # Ubuntu 22.04 LTS
+    sudo apt install wkhtmltopdf -y
+  else
+      # For older versions of Ubuntu
+    sudo gdebi --n `basename $_url`
+  fi
+  
+  sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
+  sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
+else
+  echo "Wkhtmltopdf isn't installed due to the choice of the user!"
+fi
